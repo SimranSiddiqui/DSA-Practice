de4fcc9cfc880092c2f5215c50e0acc96ad66ac5
Simran Siddiqui <simransidd005@gmail.com>
2022-10-04 23:20:16 +0530

    Time: 6 ms (84.67%), Space: 10.4 MB (49.23%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mp;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        
+        for(int i=0;i<n1;i++)
+        {
+            mp[nums1[i]]++;
+        }
+        
+        vector<int> ans;
+        for(int i=0;i<n2;i++)
+        {
+            if(mp.find(nums2[i]) != mp.end())
+            {
+                ans.push_back(nums2[i]);
+                mp.erase(nums2[i]);
+            } 
+        }
+        
+        return ans;
+    }
+};
