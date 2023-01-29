0689bb0a2303700fda342e93e23f67ab792bc33d
Simran Siddiqui <simransidd005@gmail.com>
2023-01-29 21:55:08 +0530

    Time: 4 ms (37.95%), Space: 5.8 MB (93.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,29 @@
 class Solution {
+    double pow(double x, int n)
+    {
+        if(n == 0)
+            return 1;
+        if(n == 1)
+            return x;
+        
+        double ans = pow(x, n/2);
+        
+        if(n%2 == 0)
+            return ans * ans;
+        else 
+            return ans * ans * x;
+    }
+    
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        
+        double ans = pow(x, abs(n));
+        
+        if(n >= 0)
+            return ans;
+        
+        else
+            return 1/ans;
+            
     }
 };
