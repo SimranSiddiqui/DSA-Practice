6b5bf93f3807d06505b0aa1ebb399d89d02bbca8
Simran Siddiqui <simransidd005@gmail.com>
2023-02-26 16:18:57 +0530

    Time: 119 ms (24.31%), Space: 35.8 MB (5.70%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         priority_queue<long long, vector<long long>, greater<long long>> pq;
-        unordered_map<long long, int> mp;
+        unordered_map<long long, bool> mp;
         
         pq.push(1);
-        mp[1]++;
+        mp[1] = true;
         
         while(!pq.empty() && n)
         {
@@ -18,17 +18,17 @@ public:
             
             if(mp.find(x*2) == mp.end())
             {
-                mp[x*2]++;
+                mp[x*2] = true;
                 pq.push(x*2);
             }
             if(mp.find(x*3) == mp.end())
             {
-                mp[x*3]++;
+                mp[x*3] = true;
                 pq.push(x*3);
             }
             if(mp.find(x*5) == mp.end())
             {
-                mp[x*5]++;
+                mp[x*5] = true;
                 pq.push(x*5);
             }
         }
