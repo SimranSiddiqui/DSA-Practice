5e713d75018d75e16bfe38ce9a8213877015310d
Simran Siddiqui <simransidd005@gmail.com>
2023-02-27 10:37:48 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (53.02%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n = jewels.length();
+        int ans =0;
+        
+        for(int i=0;i<n;i++)
+        {
+            ans += count(stones.begin(), stones.end(), jewels[i]);
+        }
+        
+        return ans;
+    }
+};
