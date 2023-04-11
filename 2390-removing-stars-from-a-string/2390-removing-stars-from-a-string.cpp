31e361cb46e3f10bba7121c919eaa4460760bc3d
Simran Siddiqui <simransidd005@gmail.com>
2023-04-11 21:09:28 +0530

    Time: 83 ms (71.86%), Space: 27.1 MB (28.68%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -6,29 +6,19 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(st.empty())
-            {
+            if(s[i] != '*')
                 st.push(s[i]);
-            }
-            else if(s[i] == '*')
-            {
+            else
                 st.pop();
-            }
-            else 
-            {
-                st.push(s[i]);
-            }
         }
         
         string ans = "";
-        while(!st.empty())
-        {
+        while(!st.empty()){
             ans += st.top();
             st.pop();
         }
         
         reverse(ans.begin(), ans.end());
-        
         return ans;
     }
 };
