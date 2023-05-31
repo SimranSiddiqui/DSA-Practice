49c7c930d26e6967b22445e22af830304a2d709b
Simran Siddiqui <simransidd005@gmail.com>
2023-05-31 14:56:43 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (46.87%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,50 +1,20 @@
 class Solution {
-    
-    int findPivot(vector<int> nums, int i, int j)
-    {
-        while(i<j)
-        {
-            if(nums[i] > nums[i+1])
-            {
-                return i;
-            }
-            i++;
-        }
-        
-        return -1;
-    }
-    
 public:
     bool check(vector<int>& nums) {
+        int diff = 0;
         int n = nums.size();
         
-        int index = findPivot(nums, 0, n-1);
-        
-        if(index == -1)
-            return true;
-        
-        int i = index;
-        int j = index+1;
-        
-        for(;i>0;i--)
+        for(int i=1;i<n;i++)
         {
             if(nums[i] < nums[i-1])
-            {
-                return false;
-            }
+                diff++;
         }
         
-         for(;j<n-1;j++)
-        {
-            if(nums[j] > nums[j+1])
-            {
-                return false;
-            }
-        }
+        if(nums[n-1] > nums[0])
+            diff++;
         
-        if(nums[j] > nums[i])
+        if(diff > 1)
             return false;
-        
         return true;
     }
 };
