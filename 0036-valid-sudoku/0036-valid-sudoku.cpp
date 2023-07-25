43b9ae7d10a9302364214e2cfa2dad899bb87922
Simran Siddiqui <simransidd005@gmail.com>
2023-07-25 11:50:44 +0530

    Time: 27 ms (49.56%), Space: 18.8 MB (54.33%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,19 +1,72 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-         unordered_set<string> seen;
-        for(int i=0; i<9; i++) {
-            for(int j=0; j<9; j++) {
-                int curr = board[i][j];
-                if(curr == '.') continue;
-                if(!seen.insert("row" + to_string(i) + "has" + to_string(curr)).second || 
-                   !seen.insert("col" + to_string(j) + "has" + to_string(curr)).second ||
-                   !seen.insert("box" + to_string(i/3) + to_string(j/3) + "has" + to_string(curr)).second
-                ) {
+        unordered_map<int, int> mp;
+        
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(mp.find(board[i][j]) != mp.end())
                     return false;
+                if(board[i][j] - '1' > 9)
+                    return false;
+                if(board[i][j] == '.')
+                    continue;
+                mp[board[i][j]]++;
+            }
+            
+            mp.clear();
+        }
+        
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(mp.find(board[j][i]) != mp.end())
+                    return false;
+                if(board[j][i] - '1' > 9)
+                    return false;
+                if(board[j][i] == '.')
+                    continue;
+                mp[board[j][i]]++;
+            }
+  
+            mp.clear();
+        }
+        
+        for(int i=0;i<9;i+=3)
+        {
+            for(int j=0;j<9;j+=3)
+            {
+                int r = i, c = j;
+                
+                while(r < i+3)
+                {
+                    c = j;
+                    while(c < j+3)
+                    {
+                        
+                        //cout << board[r][c] << " ";
+                        
+                        if(mp.find(board[r][c]) != mp.end())
+                             return false;
+                        if(board[r][c] - '0' > 9)
+                              return false;
+                        if(board[r][c] != '.')
+                         mp[board[r][c]]++;
+                        
+                        c++;
+                    }
+                    
+                    //cout << endl;
+                    r++;
                 }
+                
+                 mp.clear();
             }
         }
+        
         return true;
     }
 };
