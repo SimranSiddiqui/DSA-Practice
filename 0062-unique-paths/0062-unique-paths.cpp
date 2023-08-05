780bb594ea26e5d2993b1f410ed9c193fa36dbce
Simran Siddiqui <simransidd005@gmail.com>
2023-08-05 19:57:08 +0530

    Time: 3 ms (37.41%), Space: 6.6 MB (18.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,22 @@
 class Solution {
     
-    int func(int m, int n, vector<vector<int>>& dp)
+    int solve(int m, int n, vector<vector<int>> &dp)
     {
-        if(m == 0 && n == 0)
-            return 1;
-        if(m<0 || n<0)
+        if(!m || !n)
             return 0;
+        if(m == 1 && n == 1)
+            return 1;
         
         if(dp[m][n] != -1)
             return dp[m][n];
         
-        return dp[m][n] = func(m-1,n, dp)+func(m, n-1, dp);
+        return dp[m][n] = solve(m-1, n, dp) + solve(m, n-1, dp);
     }
     
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
-        return func(m-1, n-1, dp);
+        return solve(m, n, dp);
     }
 };
