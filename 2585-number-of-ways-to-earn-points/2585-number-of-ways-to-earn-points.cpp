0a63ca0829f4c15224d34161d803786568be75e7
Simran Siddiqui <simransidd005@gmail.com>
2023-08-11 12:00:32 +0530

    Time: 942 ms (13.82%), Space: 410.8 MB (9.64%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -2,36 +2,36 @@
 class Solution {
 public:
     
-    int solve(int target, vector<vector<int>>& types, int n,  vector<vector<long long>> &dp)
+    int solve(int target, vector<vector<int>>& types, int n, int i,  vector<vector<vector<long long>>> &dp)
     {
-        if(target == 0)
-            return 1;
-        if(target < 0 || n<0)
+        if(target < 0 || n<0 || i > types[n][0])
             return 0;
+        if(target == 0){
+            //cout << target << endl;
+            return 1;
+        }
         
-        if(dp[n][target] != -1)
-            return dp[n][target]%mod;
+        if(dp[n][target][i] != -1)
+            return dp[n][target][i]%mod;
         
         long long ans=0;
         
-       /* if(types[n][0] > 0){
-            types[n][0]--;
-            ans += solve(target-types[n][1], types, n);
-            types[n][0]++;
-        }*/
-        
-        for(int i=0;i<=types[n][0];i++)
+       /* for(int i=0;i<=types[n][0];i++)
         {
             ans += solve(target-types[n][1]*i, types, n-1, dp)%mod;
-        }
-      //  ans += solve(target, types, n-1);
+        }*/
         
-        return dp[n][target] = ans%mod;
+        ans += solve(target-types[n][1], types, n, i+1, dp)%mod;
+        ans += solve(target, types, n-1, 0, dp)%mod;
+        //cout << ans << endl;
+        
+        return dp[n][target][i] = ans%mod;
     }
     
     int waysToReachTarget(int target, vector<vector<int>>& types) {
         int n = types.size();
-        vector<vector<long long>> dp(n+1, vector<long long>(target+1, -1));
-        return solve(target, types, n-1, dp);
+        vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(target+1, vector<long long>(51, -1)));
+        int i = types[n-1][0];
+        return solve(target, types, n-1, 0, dp)%mod;
     }
 };
