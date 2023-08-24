516da2b2ec78c4f95b678bd84ae47fe81183ba1a
Simran Siddiqui <simransidd005@gmail.com>
2023-08-25 01:40:58 +0530

    Time: 1023 ms (21.17%), Space: 9.1 MB (74.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,15 @@
 class Solution {
-    
-    int func(vector<int>& nums, int i, int target)
+public:
+    int solve(vector<int> &nums, int n, int target)
     {
-        if(i < 0 && target==0)
-            return 1;
-        if(i < 0)
-            return 0;
+        if(n < 0)
+            return target == 0;
         
-        int plus = func(nums, i-1, target-nums[i]);
-        int minus = func(nums, i-1, target+nums[i]);
-        
-        return plus+minus;
+        return  solve(nums, n-1, target-nums[n]) + solve(nums, n-1, target+nums[n]);
     }
     
-public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
         int n = nums.size();
-        
-        return func(nums, n-1, target);
+        return solve(nums, n-1, target);
     }
 };
