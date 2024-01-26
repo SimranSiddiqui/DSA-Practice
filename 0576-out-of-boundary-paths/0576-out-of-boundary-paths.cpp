0d3e9d6a705bc0679c1032aa3fb26389870774a4
Simran Siddiqui <simransidd005@gmail.com>
2024-01-26 18:58:27 +0530

    Time: 7 ms (59.64%), Space: 11 MB (22.49%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,6 +1,9 @@
 #define mod 1000000007
 class Solution {
 public:
+    vector<int> dRow = {1, 0, -1, 0};
+    vector<int> dCol = {0, -1, 0, 1};
+    
     int solve(int m, int n, int maxMove, int row, int col, vector<vector<vector<int>>> &dp)
     {
         if(!maxMove)
@@ -10,10 +13,13 @@ public:
         if(dp[row][col][maxMove] != -1)
             return dp[row][col][maxMove];
         
-        int a = solve(m, n, maxMove-1, row-1, col, dp) + solve(m, n, maxMove-1, row+1, col, dp);
-        int b = solve(m, n, maxMove-1, row, col-1, dp) + solve(m, n, maxMove-1, row, col+1, dp);
+        int ans=0;
+        for(int i=0;i<4;i++)
+        {
+            ans = (ans%mod + solve(m, n, maxMove-1, row+dRow[i], col+dCol[i], dp)%mod)%mod;
+        }
         
-        return dp[row][col][maxMove] = (a%mod + b%mod)%mod;
+        return dp[row][col][maxMove] = ans%mod;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1, vector<int>(maxMove+1, -1)));
