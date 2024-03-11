4cdf7a2f22ddff6f116285bcea4901bbf0dea81b
Simran Siddiqui <simransidd005@gmail.com>
2024-03-11 23:53:20 +0530

    Time: 2 ms (53.95%), Space: 8.6 MB (17.36%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -3,8 +3,7 @@ class Solution {
     static bool comp(char a, char b)
     {
         return mp[a] < mp[b]; 
-    }
-    
+    }  
 public:
     string customSortString(string order, string s) { 
         for(int i=0;i<order.length();i++)
