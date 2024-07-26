0728597d45fc60bfefcdb484b73e52bf0068bb74
Simran Siddiqui <simransidd005@gmail.com>
2024-07-27 00:13:50 +0530

    Time: 51 ms (58.60%), Space: 30.3 MB (72.70%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,43 +1,50 @@
 class Solution {
-    
-    void dfs(int i, int j, int n, int m , vector<vector<int>> &grid)
-    {
-        if(i < 0 || j < 0 || i >= n || j >= m)
-            return;
-        if(grid[i][j] == 0)
-            return;
-        
-        grid[i][j] = 0;
-        
-        dfs(i-1, j, n, m, grid);
-        dfs(i+1, j, n, m, grid);
-        dfs(i, j-1, n, m, grid);
-        dfs(i, j+1, n, m, grid);
-    }
-    
 public:
+    
+    vector<int> dRow = {-1, 0, 1, 0};
+    vector<int> dCol = {0, 1, 0, -1};
+
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int count =0;
+        
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if((i==0 || j == 0 || i == n-1 || j == m-1) && grid[i][j] == 1)
-                {
-                    dfs(i, j, n, m, grid);
+                if((i == 0 || j==0 || i == n-1 || j == m-1) && grid[i][j] == 1 && vis[i][j] == 0){
+                    q.push({i, j});
+                    vis[i][j] = 1;
                 }
             }
         }
         
+        while(!q.empty()){
+            pair<int,int> curr = q.front();
+            q.pop();
+            
+            int r = curr.first, c = curr.second;
+            vis[r][c] = 1;
+            
+            for(int i=0;i<4;i++)
+            {
+                if(r+dRow[i] >= 0 && r+dRow[i] < n && c + dCol[i] >= 0 && c + dCol[i] < m && vis[r+dRow[i]][c + dCol[i]] == 0 && grid[r+dRow[i]][c + dCol[i]] == 1)
+                {
+                    q.push({r+dRow[i], c + dCol[i]});
+                    vis[r+dRow[i]][c + dCol[i]] = 1;
+                }
+            }
+        }
         
-        int count =0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j])
+                if(grid[i][j] == 1 && vis[i][j] == 0)
                     count++;
             }
         }
